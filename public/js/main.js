e37570baf3d08da86e21591f4d1ff9d9f95f15df
Yoann Picquenot <yoann.picquenot@epitech.eu>
2017-04-07 01:35:24 +0000

    adll

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
 (function() {
+    var backgroundColors = {
+        'home': '#2980b9',
+        'skills': '#27ae60',
+        'experiences': '#16a085'
+    };
+
     function loadHtml(url, cb) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -16,12 +22,21 @@
         for (i = 0; i < links.length; i++) {
             links[i].getElementsByTagName('a')[0].addEventListener('click', function(e) {
                 e.preventDefault();
+                if (e.target.pathname === window.location.pathname) {
+                    return;
+                }
                 window.history.pushState({
                         urlPath: 'views/' + (e.target.pathname.match(/\/([a-z]*)/)[1] || 'home') + '.html'
                     },
                     e.target.pathname.match(/\/([a-z]*)/)[1], e.target.pathname);
                 loadHtml(window.history.state.urlPath, function(html) {
-                    document.getElementsByTagName('main')[0].innerHTML = html;
+                    var main = document.getElementsByTagName('main')[0];
+                    document.getElementsByTagName('main')[0].style.opacity = '0';
+                    setTimeout(function() {
+                        main.innerHTML = html;
+                        main.style.opacity = '1';
+                        document.getElementsByTagName('body')[0].style.backgroundColor = backgroundColors[window.history.state.urlPath.match(/\/([a-z]*)/)[1] || 'home'];
+                    }, 500);
                 });
             });
         }
@@ -32,6 +47,10 @@
             urlPath: 'views/' + (window.location.pathname === '/' ? 'home' : window.location.pathname) + '.html'
         }, 'Title', window.location.pathname);
         loadHtml(window.history.state.urlPath, function(html) {
+            var body = document.getElementsByTagName('body')[0];
+            body.style.backgroundColor = backgroundColors[window.location.pathname.match(/\/([a-z]*)/)[1] || 'home'];
+            body.style.transition = 'background-color 1s';
+            body.style.display = 'block';
             document.getElementsByTagName('main')[0].innerHTML = html;
         });
         return;
@@ -41,7 +60,32 @@
         document.getElementsByClassName('footer-date')[0].innerHTML = new Date().getFullYear();
     }
 
-    setFooter();
+    function initMenuIcon() {
+        document.getElementsByClassName('menu-icon')[0].className += ' close';
+        document.getElementsByClassName('menu-icon')[0].addEventListener('click', function() {
+            var el = this;
+
+            if (el.classList.contains('close') !== false) {
+                el.classList.remove('close');
+                el.className += ' opening';
+                setTimeout(function() {
+                    el.classList.remove('opening');
+                    el.className += ' open';
+                }, 1400);
+            } else {
+                el.classList.remove('open');
+                el.className += ' closing';
+                setTimeout(function() {
+                    el.classList.remove('closing');
+                    el.className += ' close';
+                }, 1400)
+            }
+
+        });
+    }
+
     setUrl();
     setHrefEvents();
+    setFooter();
+    initMenuIcon();
 })();
